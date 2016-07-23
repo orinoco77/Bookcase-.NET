839fff78c4b3d761e26acf6daa3ff4ec97dfaced
orinoco77 <ajs@shiny.org.uk>
2016-07-23 12:28:11 +0100

    VS missed some files for some reason

---
+++ Bookcase.Model/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcase.Model
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public IList<Ebook> Ebooks { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Forename, Surname);
+        }
+    }
+}
---
+++ Bookcase.Model/Ebook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcase.Model
+{
+    public class Ebook
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IList<Author> Authors { get; set; }
+        public string EbookUrl { get; set; }
+        public ImageData ImageData { get; set; }
+    }
+}
